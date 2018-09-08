e46a1607a5641c7d909fc396d7e1a651c849c701
Shuo Wang <gken90@gmail.com>
2018-09-08 21:49:02 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -5,7 +5,7 @@ def my_select(collection)
   array = []
   i = 0
   while i < collection.length
-    array << (yield == true)
+    array << (yield collection[i] == true)
     i += 1
   end
     array
