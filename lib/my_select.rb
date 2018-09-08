d68aaf6956ff0f45bf0a8461bce01a80f4562b60
Shuo Wang <gken90@gmail.com>
2018-09-08 20:50:00 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,10 +1,10 @@
 def my_select(collection)
- i = 0 
- while i < collection.length 
-   yield 
-   i += 1 
- end 
+ i = 0
+ while i < collection.length
+   yield
+   i += 1
+ end
 end
 
 
-[1,2,3,4,5].select { |num|  num.even?  }   #=> [2, 4]
+# [1,2,3,4,5].select { |num|  num.even?  }   #=> [2, 4]
