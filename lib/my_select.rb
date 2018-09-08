cfe739dbf38d687254ee5ab00cf9ab176be0ef00
Shuo Wang <gken90@gmail.com>
2018-09-08 22:07:40 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,18 @@
 def my_select(collection)
- # your code here!
+  if collection == []
+    puts "This block should not run!"
+  else
+  array = []
+  i = 0
+  while i < collection.length
+    if (yield collection[i]) ==true
+    array << (collection[i])
+  end
+    i += 1
 end
+    p array
+  end
+end
+
+
+# [1,2,3,4,5].select { |num|  num.even?  }   #=> [2, 4]
